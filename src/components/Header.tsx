39bceba415c4086d6e1ad3e4d367a941c5ee9425
Alysson Cruz <alysson.cruz@alumisoft.com.br>
2024-04-11 16:19:05 -0300

    feat: socials icons added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
-import { AppBar, Toolbar, Typography } from "@mui/material";
+import { GitHub, X } from "@mui/icons-material";
+import { AppBar, Box, Link, Stack, Toolbar, Typography } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 
 export function Header() {
@@ -7,17 +8,44 @@ export function Header() {
   return (
     <AppBar position="static">
       <Toolbar variant="dense">
-        <img
-          src="/assets/standunited2.png"
-          alt="Stan United Icon"
-          width={"32px"}
-          height={"32px"}
-          style={{ borderRadius: 8, cursor: "pointer" }}
-          onClick={() => navigate("/")}
-        />
-        <Typography variant="h6" component="div" ml={2}>
-          TFT - Stand United/Built Different comp generator
-        </Typography>
+        <Box
+          display={"flex"}
+          justifyContent={"space-between"}
+          alignItems={"center"}
+          width={"100%"}
+        >
+          <Box display={"flex"}>
+            <img
+              src="/assets/standunited2.png"
+              alt="Stan United Icon"
+              width={"32px"}
+              height={"32px"}
+              style={{ borderRadius: 8, cursor: "pointer" }}
+              onClick={() => navigate("/")}
+            />
+            <Typography variant="h6" component="div" ml={2}>
+              TFT - Stand United/Built Different comp generator
+            </Typography>
+          </Box>
+          <Stack spacing={1} direction={"row"}>
+            <Link
+              sx={{ color: "black" }}
+              href="https://github.com/AlyssonT"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <GitHub />
+            </Link>
+            <Link
+              sx={{ color: "black" }}
+              href="https://twitter.com/AlyssonT5"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              <X />
+            </Link>
+          </Stack>
+        </Box>
       </Toolbar>
     </AppBar>
   );
