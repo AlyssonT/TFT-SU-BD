9c0c8426fba36e0ccfb10f68b04c1bb07a729ea0
Alysson Cruz <alysson.cruz@alumisoft.com.br>
2024-05-07 17:29:23 -0300

    feat: added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
 import theme from "./styles/theme.ts";
 import { ThemeProvider } from "@mui/material";
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <ThemeProvider theme={theme}>
     <App />
+    <Analytics />
   </ThemeProvider>
 );
